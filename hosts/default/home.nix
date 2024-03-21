bea95b70b821ac8317f6d0ea67cf03afb6a72a5d
Syed Ahkam <smahkam57@gmail.com>
2024-03-22 01:49:59 +0530

    package: add rustup

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -31,6 +31,8 @@
     pkgs.spotify
     pkgs.syncthingtray
 
+    pkgs.rustup
+
     # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
