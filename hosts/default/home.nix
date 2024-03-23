e0083249c61a41cb7d2ff51c8b4e012b7f3739ce
Syed Ahkam <smahkam57@gmail.com>
2024-03-24 01:56:43 +0530

    add heroic and gamemode

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -34,6 +34,9 @@
     pkgs.rustup
     pkgs.nil # nix lsp
 
+    pkgs.heroic
+    pkgs.gamemode
+
     # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
