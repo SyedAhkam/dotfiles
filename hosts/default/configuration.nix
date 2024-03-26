5cd23cabbdd85c73b0ece70e13a92f755def1f3a
Syed Ahkam <smahkam57@gmail.com>
2024-03-27 02:04:17 +0530

    package: add docker support

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -98,7 +98,7 @@
   users.users.syed = {
     isNormalUser = true;
     description = "Syed Ahkam";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs;
       [
         kate
@@ -160,6 +160,9 @@
     dedicatedServer.openFirewall =
       true; # Open ports in the firewall for Source Dedicated Server
   };
+
+  # Enable docker
+  virtualisation.docker.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
