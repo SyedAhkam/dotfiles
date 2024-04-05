7636b8892c47742e861847484e2dbd0f229a9a26
Syed Ahkam <smahkam57@gmail.com>
2024-04-05 22:15:47 +0530

    nix: set trusted users

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -94,6 +94,9 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  # Trusted users
+  nix.settings.trusted-users = [ "root" "syed" ];
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.syed = {
     isNormalUser = true;
