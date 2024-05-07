a2fda4f3ffee5e6b60a77343d2b6c85fa5c46d6c
Syed Ahkam <smahkam57@gmail.com>
2024-05-07 20:24:08 +0530

    feat: Enable nix-ld

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -189,6 +189,9 @@
   # Enable docker
   virtualisation.docker.enable = true;
 
+  # Enable nix-ld
+  programs.nix-ld.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
