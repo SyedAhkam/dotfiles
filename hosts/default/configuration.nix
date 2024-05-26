0d4567cf4dacabbd7b888875f03fb6b85fb9504c
Syed Ahkam <smahkam57@gmail.com>
2024-05-27 01:50:51 +0530

    firewall: allow bombsquad 43210

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -207,10 +207,9 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # 43210 is used by bombsquad
+  networking.firewall.allowedTCPPorts = [ 43210 ];
+  networking.firewall.allowedUDPPorts = [ 43210 ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
