0d7d01145aa96c8e3e66ffab56ded05b6c5809d2
Syed Ahkam <smahkam57@gmail.com>
2024-05-27 01:52:43 +0530

    fix: disable auto-upgrade
    it has no effect when used in conjunction with flakes

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -220,15 +220,15 @@
   system.stateVersion = "23.11"; # Did you read the comment?
 
   # Auto upgrade
-  system.autoUpgrade = {
-    enable = true;
-    flake = inputs.self.outPath;
-    flags = [
-      "--update-input"
-      "nixpkgs"
-      "-L" # print build logs
-    ];
-    dates = "02:00";
-    randomizedDelaySec = "45min";
-  };
+  # system.autoUpgrade = {
+  #   enable = true;
+  #   flake = inputs.self.outPath;
+  #   flags = [
+  #     "--update-input"
+  #     "nixpkgs"
+  #     "-L" # print build logs
+  #   ];
+  #   dates = "02:00";
+  #   randomizedDelaySec = "45min";
+  # };
 }
